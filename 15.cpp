10c92dc54e16761227413a1887b97b98b7fda3e1
Akshay <akshaygoti23@gmail.com>
2023-06-23 08:41:51 +0530

    while loop 51 to 99 print

---
+++ 15.cpp
@@ -0,0 +1,13 @@
+//while loop 51 to 99.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=51;
+    while(i<=100)
+    {
+        cout<<" "<<i;
+        i++;
+        cout<<"\n";
+    }    
+}
